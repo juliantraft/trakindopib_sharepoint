1046954b747f256b6f9b6154f24c7ec40db063d0
Juliant Raffa <juliantraft@gmail.com>
2026-07-30 14:53:32 +0700

    encapsulate main func

--- main.py
+++ main.py
@@ -39,87 +39,95 @@ def get_logger(debug: bool = False) -> Logger:
     return logger
 
 
-args = get_args()
-SHAREPOINT_URL = 'https://tmtgroup.sharepoint.com/sites/portal_trakindo/pib/PIBDocument/Forms/AllItems.aspx?'
-SUPPORTING_DOCS_FOLDER = get_env('DOC_FOLDER')
-TEMP_FOLDER = 'TEMP_RPA'
-DOC_DATE = datetime.strptime(args.date, "%Y-%m-%d")
+def main(argv: list[str] | None = None):
+    load_dotenv()
+    args: Namespace = get_args(argv)
+    logger: Logger = get_logger(debug=args.debug)
+
+    SHAREPOINT_URL = 'https://tmtgroup.sharepoint.com/sites/portal_trakindo/pib/PIBDocument/Forms/AllItems.aspx?'
+    SUPPORTING_DOCS_FOLDER = get_env('DOC_FOLDER')
+    TEMP_FOLDER = 'TEMP_RPA'
+    DOC_DATE = datetime.strptime(args.date, "%Y-%m-%d")
 
 
-with sharepoint_bot(headless=args.headless) as bot:
-    bot.login(get_env('LOGIN_EMAIL'), get_env('LOGIN_PASSWORD'))
-    bot.load_page(SHAREPOINT_URL, '[data-automationid="appMainContent"]')
+    with sharepoint_bot(headless=args.headless) as bot:
+        bot.login(get_env('LOGIN_EMAIL'), get_env('LOGIN_PASSWORD'))
+        bot.load_page(SHAREPOINT_URL, '[data-automationid="appMainContent"]')
 
-    # Step 1 - upload supporting docs
-    files_to_upload = []
-    with scandir(SUPPORTING_DOCS_FOLDER) as entries:
-        for entry in entries:
-            if entry.is_file() and entry.name.startswith(args.nomor_aju):
-                files_to_upload.append(abspath(entry.path))
+        # Step 1 - upload supporting docs
+        files_to_upload = []
+        with scandir(SUPPORTING_DOCS_FOLDER) as entries:
+            for entry in entries:
+                if entry.is_file() and entry.name.startswith(args.nomor_aju):
+                    files_to_upload.append(abspath(entry.path))
 
-    if not files_to_upload:
-        msg = f'No supporting doc found for {args.nomor_aju} in {SUPPORTING_DOCS_FOLDER}'
-        print (msg)
-        raise NameError(msg)
-        # TODO add error handling in RPA
+        if not files_to_upload:
+            msg = f'No supporting doc found for {args.nomor_aju} in {SUPPORTING_DOCS_FOLDER}'
+            print (msg)
+            raise NameError(msg)
+            # TODO add error handling in RPA
 
-    bot.open_folder('CEISA')
+        bot.open_folder('CEISA')
 
-    if not bot.folder_exists(TEMP_FOLDER):
-        bot.create_folder(TEMP_FOLDER)
-    bot.open_folder(TEMP_FOLDER)
+        if not bot.folder_exists(TEMP_FOLDER):
+            bot.create_folder(TEMP_FOLDER)
+        bot.open_folder(TEMP_FOLDER)
 
-    if not bot.folder_is_empty():
+        if not bot.folder_is_empty():
+            bot.select_all_files()
+            bot.delete_selection()
+            bot.page.reload()
+            # Still have no idea why reload is needed.
+            # Details in dev_notes.txt
+            # TODO find out why.
+
+        bot.upload_multiple_files(files_to_upload)
+
+        # Step 2 - move main doc
+        bot.load_page(SHAREPOINT_URL, '#listTabPanel')
+        bot.open_folder('Parts')
+
+        target_file = f'{args.nomor_aju}.pdf'
+
+        result_exist = bot.search_file(target_file)
+        if (result_exist):
+            bot.sr_open_copy_menu_for_file(target_file)
+            bot.fp_move_one_level_up()
+            bot.fp_open_folder('CEISA')
+            bot.fp_open_folder(TEMP_FOLDER)
+            bot.fp_sr_finish()
+
+        # Step 3 - Edit properties and move files
+        bot.load_page(SHAREPOINT_URL, '#listTabPanel')
+        bot.open_folder('CEISA')
+        bot.open_folder(TEMP_FOLDER)
+        
+        sleep(5)
         bot.select_all_files()
-        bot.delete_selection()
-        bot.page.reload()
-        # Still have no idea why reload is needed.
-        # Details in dev_notes.txt
-        # TODO find out why.
+        bot.open_property_editor()
 
-    bot.upload_multiple_files(files_to_upload)
+        bot.pe_fill_form('KodeBilling', args.billcode)
+        bot.pe_fill_form('AJUNo', args.nomor_aju)
+        bot.pe_fill_form('Total', args.total)
+        bot.pe_save()
 
-    # Step 2 - move main doc
-    bot.load_page(SHAREPOINT_URL, '#listTabPanel')
-    bot.open_folder('Parts')
+        folder_struct = (
+            args.type,                  # Parts/PortalInput/etc
+            DOC_DATE.strftime('%Y'),    # 2025
+            DOC_DATE.strftime('%m')     # 05
+        )
 
-    target_file = f'{args.nomor_aju}.pdf'
-
-    result_exist = bot.search_file(target_file)
-    if (result_exist):
-        bot.sr_open_copy_menu_for_file(target_file)
+        bot.open_move_menu()
         bot.fp_move_one_level_up()
-        bot.fp_open_folder('CEISA')
-        bot.fp_open_folder(TEMP_FOLDER)
-        bot.fp_sr_finish()
 
-    # Step 3 - Edit properties and move files
-    bot.load_page(SHAREPOINT_URL, '#listTabPanel')
-    bot.open_folder('CEISA')
-    bot.open_folder(TEMP_FOLDER)
-    
-    sleep(5)
-    bot.select_all_files()
-    bot.open_property_editor()
+        for folder in folder_struct:
+            if bot.fp_folder_exists(folder):
+                bot.fp_open_folder(folder)
+            else:
+                bot.fp_create_folder(folder)
 
-    bot.pe_fill_form('KodeBilling', args.billcode)
-    bot.pe_fill_form('AJUNo', args.nomor_aju)
-    bot.pe_fill_form('Total', args.total)
-    bot.pe_save()
+        bot.fp_finish()
 
-    folder_struct = (
-        args.type,                  # Parts/PortalInput/etc
-        DOC_DATE.strftime('%Y'),    # 2025
-        DOC_DATE.strftime('%m')     # 05
-    )
 
-    bot.open_move_menu()
-    bot.fp_move_one_level_up()
-
-    for folder in folder_struct:
-        if bot.fp_folder_exists(folder):
-            bot.fp_open_folder(folder)
-        else:
-            bot.fp_create_folder(folder)
-
-    bot.fp_finish()
+if __name__ == '__main__':
+    main()
