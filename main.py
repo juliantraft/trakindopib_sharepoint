b92e30c9e0bb8aa3f9e1c6a6d1ae4bba65481435
Juliant Raffa <juliantraft@gmail.com>
2026-07-31 10:26:49 +0700

    add DB connection and encapsulate main logic into rpa()

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ from os import scandir
 from os.path import abspath
 from time import sleep
 
-from src.sharepoint_bot import sharepoint_bot
-from src.logging_setup import (
-    Logger,
-    enable_ntfy_logging,
-    init_logging,
-)
-from src.utils import get_env
+from pyodbc import Connection, Error as DBError, connect
 
-load_dotenv()
+from src.sharepoint_bot import sharepoint_bot
+from src.logging_setup import Logger, enable_ntfy_logging, init_logging
+from src.utils import get_env
 
 
 def get_args(argv: list[str] | None = None) -> Namespace:
@@ -28,7 +24,8 @@ def get_args(argv: list[str] | None = None) -> Namespace:
     parser.add_argument('-d', '--date' , type=str, metavar='', help='yyyy-mm-dd')
     parser.add_argument('-b', '--billcode' , type=str, metavar='', help='ex: 321250501703123')
     parser.add_argument('-s', '--total' , type=str, metavar='', help='ex: 123456789')
-    return parser.parse_args()
+    parser.add_argument('--debug', action='store_true', help='toggle debug mode')
+    return parser.parse_args(argv)
 
 
 def get_logger(debug: bool = False) -> Logger:
@@ -39,18 +36,34 @@ def get_logger(debug: bool = False) -> Logger:
     return logger
 
 
-def main(argv: list[str] | None = None):
-    load_dotenv()
-    args: Namespace = get_args(argv)
-    logger: Logger = get_logger(debug=args.debug)
+def get_db_cnxn(logger: Logger) -> Connection:
+    cnxn_str: str = (
+        f'DRIVER={get_env("DB_DRIVER")};'
+        f'SERVER={get_env("DB_HOST")};'
+        f'DATABASE={get_env("DB_DATABASE")};'
+        f'PORT={get_env("DB_PORT")};'
+        f'UID={get_env("DB_USERNAME")};'
+        f'PWD={get_env("DB_PASSWORD")};'
+        'TrustServerCertificate=yes;'
+    )
+    try:
+        db_cnxn: Connection = connect(cnxn_str)
+        logger.info(f'Connection to "{get_env("DB_DATABASE")}" established')
+        return db_cnxn
+    except DBError as e:
+        logger.critical(
+            f'Connection to "{get_env("DB_DATABASE")}" failed: {e}',
+        )
+        raise
 
+
+def rpa(args: Namespace, logger: Logger, db_cnxn: Connection) -> None:
     SHAREPOINT_URL = 'https://tmtgroup.sharepoint.com/sites/portal_trakindo/pib/PIBDocument/Forms/AllItems.aspx?'
     SUPPORTING_DOCS_FOLDER = get_env('DOC_FOLDER')
     TEMP_FOLDER = 'TEMP_RPA'
     DOC_DATE = datetime.strptime(args.date, "%Y-%m-%d")
 
-
-    with sharepoint_bot(headless=args.headless) as bot:
+    with sharepoint_bot(headless=False) as bot:
         bot.login(get_env('LOGIN_EMAIL'), get_env('LOGIN_PASSWORD'))
         bot.load_page(SHAREPOINT_URL, '[data-automationid="appMainContent"]')
 
@@ -127,6 +140,26 @@ def main(argv: list[str] | None = None):
                 bot.fp_create_folder(folder)
 
         bot.fp_finish()
+
+
+def main(argv: list[str] | None = None):
+    load_dotenv()
+    args: Namespace = get_args(argv)
+    logger: Logger = get_logger(debug=args.debug)
+    db_cnxn: Connection = get_db_cnxn(logger)
+
+    try:
+        rpa(args, logger, db_cnxn)
+    except Exception as e:
+        err_name: str = e.__class__.__name__
+        if isinstance(e, (RuntimeError,)):
+            logger.critical(f'{err_name}: {e}')
+        else:
+            logger.critical(f'Unhandled {err_name}: {e}', exc_info=(not args.debug))
+        raise
+    finally:
+        db_cnxn.close()
+        logger.info(f'Connection to "{get_env("DB_DATABASE")}" closed')
 
 
 if __name__ == '__main__':
