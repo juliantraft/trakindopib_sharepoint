589d4f3eebe02e591e0c743cffac1641f18e338a
Juliant Raffa <juliantraft@gmail.com>
2026-07-31 14:07:53 +0700

    main flow update (1)

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from argparse import ArgumentParser, Namespace, RawTextHelpFormatter
-from datetime import datetime
 from dotenv import load_dotenv
-from os import scandir
-from os.path import abspath
+from pathlib import Path
 from time import sleep
 
-from pyodbc import Connection, Error as DBError, connect
+from pyodbc import Connection, Cursor, Error as DBError, connect
 
+from src import task_manager as tm
 from src.sharepoint_bot import sharepoint_bot
-from src.logging_setup import Logger, enable_ntfy_logging, init_logging
+from src.logging_setup import Logger, enable_ntfy_logging, init_logging, add_log_context, del_log_context
 from src.utils import get_env
+
+
+SHAREPOINT_URL = 'https://tmtgroup.sharepoint.com/sites/portal_trakindo/pib/PIBDocument/Forms/AllItems.aspx?'
 
 
 def get_args(argv: list[str] | None = None) -> Namespace:
@@ -19,12 +21,7 @@ def get_args(argv: list[str] | None = None) -> Namespace:
         usage='%(prog)s [options]',
         epilog='(c) 2026 PT Pratama Natanusa Mandiri | github.com/juliantraft',
     )
-    parser.add_argument('nomor_aju', help='ex: 00000000000000001234567890')
-    parser.add_argument('-t', '--type' , type=str, metavar='', help='ex: Parts / SNP / PortalInput')
-    parser.add_argument('-d', '--date' , type=str, metavar='', help='yyyy-mm-dd')
-    parser.add_argument('-b', '--billcode' , type=str, metavar='', help='ex: 321250501703123')
-    parser.add_argument('-s', '--total' , type=str, metavar='', help='ex: 123456789')
-    parser.add_argument('--debug', action='store_true', help='toggle debug mode')
+    parser.add_argument('-d', '--debug', action='store_true', help='toggle debug mode')
     return parser.parse_args(argv)
 
 
@@ -57,89 +54,103 @@ def get_db_cnxn(logger: Logger) -> Connection:
         raise
 
 
-def rpa(args: Namespace, logger: Logger, db_cnxn: Connection) -> None:
-    SHAREPOINT_URL = 'https://tmtgroup.sharepoint.com/sites/portal_trakindo/pib/PIBDocument/Forms/AllItems.aspx?'
-    SUPPORTING_DOCS_FOLDER = get_env('DOC_FOLDER')
+def upload_doc(task: tm.PIBTask, bot: sharepoint_bot, logger: Logger) -> None:
+    SUPPORTING_DOCS_DIR = Path(get_env('DOC_FOLDER'))
     TEMP_FOLDER = 'TEMP_RPA'
-    DOC_DATE = datetime.strptime(args.date, "%Y-%m-%d")
+
+    # Find supporting docs
+    supporting_docs: list[Path] = []
+    for item in SUPPORTING_DOCS_DIR.iterdir():
+        if item.is_file() and item.name.startswith(task.data.no_aju):
+            supporting_docs.append(item)
+
+    if not supporting_docs:
+        logger.critical('No supporting doc found')
+        return
+
+    bot.open_folder('CEISA')
+
+    if not bot.folder_exists(TEMP_FOLDER):
+        bot.create_folder(TEMP_FOLDER)
+    bot.open_folder(TEMP_FOLDER)
+
+    if not bot.folder_is_empty():
+        bot.select_all_files()
+        bot.delete_selection()
+        bot.page.reload() # type: ignore
+        # Still have no idea why reload is needed.
+        # Details in dev_notes.txt
+        # TODO find out why.
+
+    bot.upload_multiple_files(supporting_docs)
+
+    # Step 2 - move main doc
+    bot.load_page(SHAREPOINT_URL, '#listTabPanel')
+    bot.open_folder('Parts')
+
+    target_file = f'{task.data.no_aju}.pdf'
+
+    result_exist = bot.search_file(target_file)
+    if (result_exist):
+        bot.sr_open_copy_menu_for_file(target_file)
+        bot.fp_move_one_level_up()
+        bot.fp_open_folder('CEISA')
+        bot.fp_open_folder(TEMP_FOLDER)
+        bot.fp_sr_finish()
+
+    # Step 3 - Edit properties and move files
+    bot.load_page(SHAREPOINT_URL, '#listTabPanel')
+    bot.open_folder('CEISA')
+    bot.open_folder(TEMP_FOLDER)
+    
+    sleep(5)
+    bot.select_all_files()
+    bot.open_property_editor()
+
+    bot.pe_fill_form('KodeBilling', task.data.bill_code)
+    bot.pe_fill_form('AJUNo', task.data.no_aju)
+    bot.pe_fill_form('Total', str(task.data.bill_total))
+    bot.pe_save()
+
+    folder_struct = (
+        task.data.type,                  # Parts/PortalInput/etc
+        task.data.trx_date.strftime('%Y'),    # 2025
+        task.data.trx_date.strftime('%m')     # 05
+    )
+
+    bot.open_move_menu()
+    bot.fp_move_one_level_up()
+
+    for folder in folder_struct:
+        if bot.fp_folder_exists(folder):
+            bot.fp_open_folder(folder)
+        else:
+            bot.fp_create_folder(folder)
+
+    bot.fp_finish()
+
+
+def rpa(logger: Logger, db_cnxn: Connection) -> None:
+    cursor: Cursor = db_cnxn.cursor()
+    tasks: list[tm.PIBTask] = tm.get_tasks(cursor, 1)
+
+    if not tasks:
+        return
+
+    for task in tasks:
+        print(task.data)
+
+    exit(1)
 
     with sharepoint_bot(headless=False) as bot:
         bot.login(get_env('LOGIN_EMAIL'), get_env('LOGIN_PASSWORD'))
         bot.load_page(SHAREPOINT_URL, '[data-automationid="appMainContent"]')
 
-        # Step 1 - upload supporting docs
-        files_to_upload = []
-        with scandir(SUPPORTING_DOCS_FOLDER) as entries:
-            for entry in entries:
-                if entry.is_file() and entry.name.startswith(args.nomor_aju):
-                    files_to_upload.append(abspath(entry.path))
+        for task in tasks:
+            add_log_context({'no_aju': task.data.no_aju})
+            upload_doc(task, bot, logger)
 
-        if not files_to_upload:
-            msg = f'No supporting doc found for {args.nomor_aju} in {SUPPORTING_DOCS_FOLDER}'
-            print (msg)
-            raise NameError(msg)
-            # TODO add error handling in RPA
-
-        bot.open_folder('CEISA')
-
-        if not bot.folder_exists(TEMP_FOLDER):
-            bot.create_folder(TEMP_FOLDER)
-        bot.open_folder(TEMP_FOLDER)
-
-        if not bot.folder_is_empty():
-            bot.select_all_files()
-            bot.delete_selection()
-            bot.page.reload()
-            # Still have no idea why reload is needed.
-            # Details in dev_notes.txt
-            # TODO find out why.
-
-        bot.upload_multiple_files(files_to_upload)
-
-        # Step 2 - move main doc
-        bot.load_page(SHAREPOINT_URL, '#listTabPanel')
-        bot.open_folder('Parts')
-
-        target_file = f'{args.nomor_aju}.pdf'
-
-        result_exist = bot.search_file(target_file)
-        if (result_exist):
-            bot.sr_open_copy_menu_for_file(target_file)
-            bot.fp_move_one_level_up()
-            bot.fp_open_folder('CEISA')
-            bot.fp_open_folder(TEMP_FOLDER)
-            bot.fp_sr_finish()
-
-        # Step 3 - Edit properties and move files
-        bot.load_page(SHAREPOINT_URL, '#listTabPanel')
-        bot.open_folder('CEISA')
-        bot.open_folder(TEMP_FOLDER)
-        
-        sleep(5)
-        bot.select_all_files()
-        bot.open_property_editor()
-
-        bot.pe_fill_form('KodeBilling', args.billcode)
-        bot.pe_fill_form('AJUNo', args.nomor_aju)
-        bot.pe_fill_form('Total', args.total)
-        bot.pe_save()
-
-        folder_struct = (
-            args.type,                  # Parts/PortalInput/etc
-            DOC_DATE.strftime('%Y'),    # 2025
-            DOC_DATE.strftime('%m')     # 05
-        )
-
-        bot.open_move_menu()
-        bot.fp_move_one_level_up()
-
-        for folder in folder_struct:
-            if bot.fp_folder_exists(folder):
-                bot.fp_open_folder(folder)
-            else:
-                bot.fp_create_folder(folder)
-
-        bot.fp_finish()
+        del_log_context('no_aju')
 
 
 def main(argv: list[str] | None = None):
@@ -149,7 +160,7 @@ def main(argv: list[str] | None = None):
     db_cnxn: Connection = get_db_cnxn(logger)
 
     try:
-        rpa(args, logger, db_cnxn)
+        rpa(logger, db_cnxn)
     except Exception as e:
         err_name: str = e.__class__.__name__
         if isinstance(e, (RuntimeError,)):
